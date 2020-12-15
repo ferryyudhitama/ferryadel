e219ae3c2263a93272e4ba720ca55d2630ea850b
Ferry Yudhitama <ferryyudhitama@gmail.com>
2020-12-15 23:36:54 +0800

    change title

--- js/script.js
+++ js/script.js
@@ -698,6 +698,28 @@
         //$('.surface-shader')
     }
 
+    function parse_query_string(query) {
+        var vars = query.split("&");
+        var query_string = {};
+        for (var i = 0; i < vars.length; i++) {
+          var pair = vars[i].split("=");
+          var key = decodeURIComponent(pair[0]);
+          var value = decodeURIComponent(pair[1]);
+          // If first entry with this name
+          if (typeof query_string[key] === "undefined") {
+            query_string[key] = decodeURIComponent(value);
+            // If second entry with this name
+          } else if (typeof query_string[key] === "string") {
+            var arr = [query_string[key], decodeURIComponent(value)];
+            query_string[key] = arr;
+            // If third or later entry with this name
+          } else {
+            query_string[key].push(decodeURIComponent(value));
+          }
+        }
+        return query_string;
+      }
+
 
 
      /*==========================================================================
@@ -719,6 +741,16 @@
             }
 
             smoothScrolling($("#navbar > ul > li > a[href^='#']"), $(".header-style-1 .navigation").innerHeight());
+
+            var query = window.location.search.substring(1);
+            var qs = parse_query_string(query);
+    
+            if(query.length !== 0){
+                $('#toPeople').text('to: ' + qs.name);
+            }else{
+                $('#toPeople').text('');
+            }
+   
 
         });
 
