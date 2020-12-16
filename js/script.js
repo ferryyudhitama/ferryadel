a3e4d43a386e2607ef0e9303d209d625d37d3e09
Ferry Yudhitama <ferryyudhitama@gmail.com>
2020-12-16 20:35:15 +0800

    asfsadf232

--- js/script.js
+++ js/script.js
@@ -822,13 +822,13 @@
     //     document.documentElement.style.setProperty('--vh', `${vh}px`);
     // });
 
-    function appHeight() {
-        const doc = document.documentElement
-        doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
-    }
+    // function appHeight() {
+    //     const doc = document.documentElement
+    //     doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
+    // }
     
-    window.addEventListener('resize', appHeight);
-    appHeight();
+    // window.addEventListener('resize', appHeight);
+    // appHeight();
 
 
 
